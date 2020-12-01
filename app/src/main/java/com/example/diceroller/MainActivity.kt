37b47af4008a3852ab5a90141e4d489e6811b1e8
swati <solankiswati71@gmail.com>
2020-12-01 13:37:55 +0530

    Initial Commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun rollDice() {
         val imageRes = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
